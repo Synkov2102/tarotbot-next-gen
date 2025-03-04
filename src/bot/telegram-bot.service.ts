40c2d06958537b296195236f65ccdbaeb5e1cc16
k.alexandr <k.alexandr@codewizard.team>
2025-03-04 23:13:02 +0300

    feat: Integrate Tarot card functionality into Telegram bot

--- src/bot/telegram-bot.service.ts
+++ src/bot/telegram-bot.service.ts
@@ -1,13 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import TelegramBot from 'node-telegram-bot-api';
 import { AiService } from 'src/ai/ai.service';
+import { TarotService } from 'src/tarot/tarot.service';
 
 @Injectable()
 export class TelegramBotService {
   private token = process.env.TELEGRAM_BOT_TOKEN; // Используем переменную окружения
   private bot: TelegramBot;
 
-  constructor(private aiService: AiService) {
+  constructor(
+    private aiService: AiService,
+    private readonly tarotService: TarotService,
+  ) {
     if (!this.token) {
       throw new Error('TELEGRAM_BOT_TOKEN environment variable is not set');
     }
@@ -37,15 +41,37 @@ export class TelegramBotService {
    */
   private async handleMessage(msg: TelegramBot.Message) {
     const chatId = msg.chat.id;
-    const text = msg.text;
+    const text = msg.text ?? '';
 
-    const ai = await this.aiService.sendMessage([
+    // Тестирование AI и DB
+    Logger.warn(text);
+
+    const card = await this.tarotService.getRandomCard();
+    const response = await this.aiService.generateAiResponse([
       {
         role: 'system',
+        content:
+          'Ты профессиональный гадальщик и таролог. Ответь на вопрос пользователя, используя данные о карте, которая будет выбрана случайным образом. Сделай небольшое предсказание в несколько предложение',
+      },
+      {
+        role: 'user',
         content: text,
+      },
+      {
+        role: 'assistant',
+        content: `
+        Название карты: ${card.name}
+        Ссылка на изображение карты: ${card.image_url}
+        Базовая интерпретация карты: ${card.base_interpretation_upright}
+        Ключевые слова: ${card.keywords_upright}
+        Символика: ${card.symbolism}
+        Астрологическая ассоциация: ${card.astrological_association}
+        `,
       },
     ]);
 
-    await this.bot.sendMessage(chatId, ai?.choices[0]?.message.content);
+    await this.bot.sendMessage(chatId, card.name);
+    await this.bot.sendPhoto(chatId, card.image_url ?? '');
+    await this.bot.sendMessage(chatId, response);
   }
 }
