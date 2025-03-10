cabf2cff04a3917b6c098f002badb29ca6e0eacc
Сынков Евгений <synkoveugeny@gmail.com>
2025-03-10 19:48:11 +0300

    feat: thre cards

--- src/bot/telegram-bot.service.ts
+++ src/bot/telegram-bot.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import TelegramBot from 'node-telegram-bot-api';
+import { AiProviderBrand } from 'src/ai/ai.interface';
 import { AiService } from 'src/ai/ai.service';
 import { TarotService } from 'src/tarot/tarot.service';
 
@@ -46,32 +47,69 @@ export class TelegramBotService {
     // Тестирование AI и DB
     Logger.warn(text);
 
-    const card = await this.tarotService.getRandomCard();
-    const response = await this.aiService.generateAiResponse([
+    const cards = await this.tarotService.getThreeRandomCards();
+
+    const cardsText = cards
+      .map((card) => {
+        const isReversed = card.position === 'reversed';
+
+        return `Название карты: ${card.name}
+        Ссылка на изображение карты: ${card.image_url ?? 'Нет изображения'}
+        Базовая интерпретация карты: ${isReversed ? card.base_interpretation_reversed : card.base_interpretation_upright}
+        Ключевые слова: ${isReversed ? card.keywords_reversed : card.keywords_upright}
+        Символика: ${card.symbolism ?? 'Нет данных'}
+        Астрологическая ассоциация: ${card.astrological_association ?? 'Нет данных'}
+        Положение карты: ${isReversed ? 'Перевёрнутое' : 'Прямое'}
+        `;
+      })
+      .join('\n-----------------\n');
+
+    const promt = [
       {
         role: 'system',
         content:
-          'Ты профессиональный гадальщик и таролог. Ответь на вопрос пользователя, используя данные о карте, которая будет выбрана случайным образом. Сделай небольшое предсказание в несколько предложение',
+          'Ты профессиональный таролог. Ответь на вопрос пользователя, используя данные о карте, которая будет выбрана случайным образом. Сделай небольшое предсказание в несколько предложение' +
+          'По контексту вопроса необходимо выбрать схему расклада и в соответствии с ней трактовать карты. Какие расклады Таро возможны: Ваше прошлое (карта 1), ваше настоящее (карта 2) и ваше будущее (карта 3); Вы (1), ваш жизненный путь (2), ваш потенциал (3); Вы (1), ваши отношения (2), ваш партнер (3); Ситуация (1), действие (2), исход (3); Идея (1), процесс (2) и стремление (3).' +
+          'Выпали следующие карты в таком порядке: ' +
+          cardsText,
       },
       {
         role: 'user',
         content: text,
       },
-      {
-        role: 'assistant',
-        content: `
-        Название карты: ${card.name}
-        Ссылка на изображение карты: ${card.image_url}
-        Базовая интерпретация карты: ${card.base_interpretation_upright}
-        Ключевые слова: ${card.keywords_upright}
-        Символика: ${card.symbolism}
-        Астрологическая ассоциация: ${card.astrological_association}
-        `,
-      },
-    ]);
+    ];
 
-    await this.bot.sendMessage(chatId, card.name);
-    await this.bot.sendPhoto(chatId, card.image_url ?? '');
-    await this.bot.sendMessage(chatId, response);
+    const GigaResponse = await this.aiService.generateAiResponse(
+      promt,
+      AiProviderBrand.GIGACHAT,
+    );
+
+    const DepSeekResponse = await this.aiService.generateAiResponse(
+      promt,
+      AiProviderBrand.DEEPSEEK,
+    );
+
+    // Отправляем информацию по каждой карте по очереди
+    for (const card of cards) {
+      await this.bot.sendMessage(chatId, `Карта: ${card.name}`);
+
+      if (card.image_url) {
+        await this.bot.sendPhoto(chatId, card.image_url);
+      }
+
+      await this.bot.sendMessage(
+        chatId,
+        `Базовая интерпретация: ${card.base_interpretation_upright}`,
+      );
+
+      await this.bot.sendMessage(
+        chatId,
+        `Астрологическая ассоциация: ${card.astrological_association ?? 'Нет данных'}`,
+      );
+    }
+
+    // Отправляем AI ответы
+    await this.bot.sendMessage(chatId, 'GigaChat: ' + GigaResponse);
+    await this.bot.sendMessage(chatId, 'DeepSeek: ' + DepSeekResponse);
   }
 }
